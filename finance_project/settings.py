f057273a1d31adfbd6e90d1f3710e7b098b8b251
NikitkaGoncharov <atpj@mail.ru>
2025-10-24 00:48:23 +0300

    - Configure Django settings for templates and static files

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t)!ut2@hb-mepqd6j@4nno-h(ud8^ht68*%^rg*hf$bor&d-vk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'finance_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
